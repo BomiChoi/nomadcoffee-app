9d5969798cc8f0dbdd270f9cc448e1dec8b43784
Bomi Choi <hibomi97@gmail.com>
2021-08-23 22:34:12 +0900

    Challenge 11: AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import { Asset } from 'expo-asset';
+import * as Font from "expo-font";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map(font => Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/logo.png"), "https://upload.wikimedia.org/wikipedia/commons/2/2a/Instagram_logo.svg"]
+    const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image));
+    console.log(fontPromises);
+    console.log(imagePromises);
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
